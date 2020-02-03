6eac22b1dd0c24f03ba203853d1b64bcd15a9079
Michael Berger <michael.anthony.berger@gmail.com>
2020-02-02 19:39:42 -0500

    Finished player movement

--- LanternLights/Assets/Scripts/PlayerMover.cs
+++ LanternLights/Assets/Scripts/PlayerMover.cs
@@ -6,48 +6,164 @@ public class PlayerMover : MonoBehaviour
 {
     // Public Properties
     public PlayerAnim animScript;
-    public float speed = 5f;
+    public float maxRunSpeed = 5f;
+    public float secondsToMaxSpeed = 0.1f;
+    public float groundedSecondsToStop = 0.1f;
+    public float airborneSecondsToStop = 0.5f;
+    public float jumpVelocity = 15f;
+    public bool debug = false;
+    public GameObject debugPrefab1;
+    public GameObject debugPrefab2;
 
     // Private Properties
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rigid;
+    private CapsuleCollider2D capsuleCollider;
+    private BoxCollider2D boxTrigger;
+    private Vector2 lastVelocity;
+    private Vector3 slopeAxis = Vector3.right;
+    private float runTimeStart = 0f;
+    private float stopTimeStart = 0f;
+    private float decelTime = 0f;
+    private float accelerationMultiplier = 0f;
+    private float decelerationMultiplier = 0f;
+    private float lastAxisX = 0f;
     private float lastAxisY = 0f;
+    private float startingGravity;
+    private bool grounded = false;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start()
+    /// </summary>
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        boxTrigger = GetComponent<BoxCollider2D>();
+        startingGravity = rigid.gravityScale;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update()
+    /// </summary>
     void Update()
     {
         // Getting the input values
         float axisX = Input.GetAxisRaw("Horizontal");
         float axisY = Input.GetAxisRaw("Vertical");
 
-        // Movement
-        if (axisX > 0f)
+        // IF there is new running input...
+        if (axisX != 0f && axisX != lastAxisX)
         {
-            rigidbody.AddForce(Vector2.right * Time.deltaTime * speed * 100f);
-            animScript.MoveRight();
-            // rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, 10f);
-        }
-        else if (axisX < 0f)
-        {
-            rigidbody.AddForce(Vector2.left * Time.deltaTime * speed * 100f);
-            animScript.MoveLeft();
-            // rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, 10f);
-        }
-        else
-        {
-            animScript.StandStill();
-        }
-        if (axisY > 0f && axisY != lastAxisY)
-        {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 10f);
+            runTimeStart = Time.time;
         }
 
-        //
+        // IF there is new stopping input...
+        if (axisX == 0f && axisX != lastAxisX)
+        {
+            stopTimeStart = Time.time;
+        }
+
+        // Calculating the deceleration multiplier
+        decelTime = (grounded) ? groundedSecondsToStop : airborneSecondsToStop;
+        decelerationMultiplier = Mathf.Clamp((stopTimeStart + decelTime - Time.time) / decelTime, 0f, 1f);
+
+        // Running
+        if (axisX != 0f && Mathf.Floor(rigid.velocity.x) != Mathf.Floor(maxRunSpeed * axisX))
+        {
+            // Calculating the movement vector
+            float moveX = axisX * maxRunSpeed * (Time.deltaTime / secondsToMaxSpeed);
+
+            // IF the player is grounded, follow the slope axis, otherwise do NOT do that
+            if (grounded)
+            {
+                Vector2 moveVector = new Vector2(slopeAxis.x * moveX, slopeAxis.y * moveX);
+                rigid.velocity += moveVector;
+            }
+            else
+                rigid.velocity += new Vector2(moveX, 0f);
+        }
+
+        // IF the player is not holding down a button (but are still moving)...
+        if (axisX == 0f && rigid.velocity.x != 0f)
+        {
+            // IF the player is grounded, decelerate along the slope axis
+            if (grounded)
+                rigid.velocity *= slopeAxis * decelerationMultiplier;
+            else
+                rigid.velocity *= new Vector2(decelerationMultiplier, 1f);
+        }
+
+        // Jumping
+        if (axisY > 0f && axisY != lastAxisY && grounded)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, jumpVelocity);
+            grounded = false;
+        }
+
+        // ANIMATING
+        if (axisX > 0f)
+            animScript.MoveRight();
+        else if (axisX < 0f)
+            animScript.MoveLeft();
+        else
+            animScript.StandStill();
+
+        // Setting the "Last Run Time" variables
+        lastAxisX = axisX;
         lastAxisY = axisY;
+        lastVelocity = rigid.velocity;
+    }
+
+    /// <summary>
+    /// OnTriggerEnter()
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // IF the other collider is a Platform...
+        if (other.gameObject.tag == "Platform")
+        {
+            // Casting to the correct collider type
+            BoxCollider2D otherBox = (BoxCollider2D) other;
+
+            // Get the closest point to the player
+            Vector3 closestPoint = otherBox.ClosestPoint(transform.position);
+
+            // IF the closest point is on the ground...
+            if (closestPoint.y <= transform.position.y - boxTrigger.bounds.extents.y / 2)
+            {
+                // Telling the player it has been grounded
+                grounded = true;
+
+                // Getting the new slope axis
+                slopeAxis = transform.position - closestPoint;
+                slopeAxis = new Vector3(slopeAxis.y, -slopeAxis.x, 0f);
+                slopeAxis.Normalize();
+
+                // Disable gravity on the rigidbody
+                rigid.gravityScale = 0f;
+            }
+
+            if (debug)
+                if (grounded)
+                    Instantiate(debugPrefab1, closestPoint, Quaternion.identity);
+                else
+                    Instantiate(debugPrefab2, closestPoint, Quaternion.identity);
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerExit2D()
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // IF the other collider is a Platform...
+        if (other.gameObject.tag == "Platform")
+        {
+            // Enable gravity
+            rigid.gravityScale = startingGravity;
+            grounded = false;
+        }
     }
 }
